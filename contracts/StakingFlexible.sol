d10d8939b1b2d0d2c6d927ad7c238e9cab19067d
duong.nt <myusername@myemaildomain.com>
2022-11-30 17:05:02 +0700

    #fix smc flexible

--- contracts/StakingFlexible.sol
+++ contracts/StakingFlexible.sol
@@ -7,7 +7,9 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "./interfaces/IStakingFlexible.sol";
 
 contract StakingFlexible is Ownable, IStakingFlexible {
-
+    
+    using Counters for Counters.Counter;
+    Counters.Counter public ID;
     mapping(address => StakeInfo[]) private listStakeUser;
     
     IERC20 public token;
@@ -33,7 +35,7 @@ contract StakingFlexible is Ownable, IStakingFlexible {
         emit SendRewardByAdmin(msg.sender, _amount, block.timestamp);
     }
 
-    function withdrawToken(uint256 _amount) public override onlyOwner {
+    function withdrawTokenAdmin(uint256 _amount) public override onlyOwner {
         require(_amount <= totalRewardPool, "not enough token to withdraw");
         token.transfer(msg.sender, _amount);
         totalRewardPool -= _amount;
@@ -41,11 +43,11 @@ contract StakingFlexible is Ownable, IStakingFlexible {
     }
 
 // private function
-    function removeItemArray(address _user, uint256 _index) private {
+    function _removeItemArray(address _user, uint256 _ID) private {
         uint256 size = listStakeUser[_user].length;
 
         for(uint256 i = 0; i < size; i ++) {
-            if(i == _index) {
+            if(_ID == listStakeUser[_user][i].ID) {
                 listStakeUser[_user][i] = listStakeUser[_user][size - 1];
                 listStakeUser[_user].pop();
                 break;
@@ -53,34 +55,58 @@ contract StakingFlexible is Ownable, IStakingFlexible {
         }
     }
 
-    function _claimReward(address _user, uint256 _index, uint256 _claimTime) private {
-        StakeInfo memory dataUser = listStakeUser[_user][_index];
+    function _findStake(address _account, uint256 _ID) view private returns(StakeInfo storage){
+        for(uint256 i = 0;i < listStakeUser[_account].length; i++){
+            if(_ID == listStakeUser[_account][i].ID){
+                return listStakeUser[_account][i];
+            }
+        }
+        revert('Not found');
+    }
 
-        uint256 bonus = (dataUser.amount * dataUser.apr * (_claimTime - dataUser.startTime)) / (minTimeToReward * divisor);
+    function _claimReward(address _user, uint256 _ID, uint256 _claimTime) private {
+        StakeInfo storage dataUser = _findStake(_user, _ID);
+        uint256 calTime = (_claimTime - dataUser.timeClaimed) / minTimeToReward;
+        uint256 bonus = (dataUser.amount * dataUser.apr * calTime) / divisor;
         require(bonus <= totalRewardPool, "not enough token to reward, please notify admin");
-        token.transfer(_user, bonus + dataUser.amount);
-        removeItemArray(_user, _index);
+        token.transfer(_user, bonus);
+        dataUser.timeClaimed = _claimTime;
         totalRewardPool -= bonus;
 
-        emit ClaimReward(_user, bonus + dataUser.amount, _claimTime);
+        emit ClaimReward(_user, bonus, _claimTime);
     }    
+
+    function _withdrawTokenStake(address _user, uint256 _ID, uint256 _claimTime) private {
+        StakeInfo storage dataUser = _findStake(_user, _ID);
+        uint256 calTime = (_claimTime - dataUser.timeClaimed) / minTimeToReward;
+        uint256 bonus = (dataUser.amount * dataUser.apr * calTime) / divisor;
+        require(bonus <= totalRewardPool, "not enough token to reward, please notify admin");
+        token.transfer(_user, bonus + dataUser.amount);
+        totalRewardPool -= bonus;
+        _removeItemArray(_user, _ID);
+
+        emit WithdrawTokenStake(_user, dataUser.amount + bonus, _claimTime);
+    }
 
     // user function
     function stakeToken(uint256 _amount) public override {
-        require(token.allowance(msg.sender, address(this)) >= _amount, "not enough token approve to stake");
         token.transferFrom(msg.sender, address(this), _amount);
-        StakeInfo memory dataStake = StakeInfo(_amount, apr, block.timestamp);
+        uint256 time = block.timestamp;
+        StakeInfo memory dataStake = StakeInfo(_amount, apr, time, time, ID.current());
+        ID.increment();
         listStakeUser[msg.sender].push(dataStake);
-        emit Stake(msg.sender, _amount, block.timestamp , apr);
+        emit Stake(msg.sender, _amount, block.timestamp , apr, dataStake.ID);
     }
 
-    function claimReward(uint256 _index) public override {
-        uint256 size = listStakeUser[msg.sender].length;
-        require (_index < size, "index incorrect");
+    function claimReward(uint256 _ID) public override {
         uint256 claimTime = block.timestamp;
+        _claimReward(msg.sender, _ID, claimTime);
 
-        _claimReward(msg.sender, _index, claimTime);
-        
+    }
+
+    function withdrawTokenStake(uint256 _ID) public override {
+        uint256 claimTime = block.timestamp;
+        _withdrawTokenStake(msg.sender, _ID, claimTime);
     }
 
     // view function
@@ -96,13 +122,11 @@ contract StakingFlexible is Ownable, IStakingFlexible {
         return listStakeUser[_user];
     }
 
-    function viewAmountBonusCurrent(uint256 _index) public view override returns(uint256) {
-        uint256 size = listStakeUser[msg.sender].length;
-        require (_index < size, "index incorrect");
-        StakeInfo memory dataUser = listStakeUser[msg.sender][_index];
-        uint256 claimTime = block.timestamp;
-        uint256 bonus = (dataUser.amount * dataUser.apr * (claimTime - dataUser.startTime)) / (minTimeToReward * divisor);
-    
+    function viewAmountBonusCurrent(address _user, uint256 _ID) public view override returns(uint256) {
+        StakeInfo memory dataUser = _findStake(_user, _ID);
+        uint256 time = block.timestamp;
+        uint256 calTime = (time - dataUser.timeClaimed) / minTimeToReward;
+        uint256 bonus = (dataUser.amount * dataUser.apr * calTime) / divisor;
         return bonus;
     }
 
--- contracts/interfaces/IStakingFlexible.sol
+++ contracts/interfaces/IStakingFlexible.sol
@@ -7,10 +7,13 @@ interface IStakingFlexible {
         uint256 amount;
         uint256 apr;
         uint256 startTime;
+        uint256 timeClaimed;
+        uint256 ID;
     }
 
-  event Stake(address user, uint256 _amount, uint256 _startTime, uint256 rate);
+  event Stake(address user, uint256 _amount, uint256 _startTime, uint256 rate, uint256 ID);
   event ClaimReward(address user, uint256 _amount, uint256 _claimTime);
+  event WithdrawTokenStake(address user, uint256 _amount, uint256 _claimTime);
   event SendRewardByAdmin(address user, uint256 _amount, uint256 timestamp);
   event WithdrawnByAdmin(address user, uint256 _amount, uint256 timestamp);
   event SetApr(address admin, uint256 apr, uint256 timestamp);
@@ -21,13 +24,15 @@ interface IStakingFlexible {
 
     function sendTokenReward(uint256 _amount) external;
 
-    function withdrawToken(uint256 _amount) external;
+    function withdrawTokenAdmin(uint256 _amount) external;
 
     // user function
 
     function stakeToken(uint256 _amount) external;
 
     function claimReward(uint256 _index) external;
+
+    function withdrawTokenStake(uint256 _index) external;
 
     // view function
 
@@ -37,6 +42,6 @@ interface IStakingFlexible {
 
     function showListStakeUser(address _user) external view returns(StakeInfo[] memory);
 
-    function viewAmountBonusCurrent(uint256 _index) external view returns(uint256);
+    function viewAmountBonusCurrent(address _user, uint256 _ID) external view returns(uint256);
 
 }
